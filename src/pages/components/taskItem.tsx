0e8a4f52be5d3a38e842dfcf1dd7871015b5df59
mostafa saffari <mostafasaffari@gmail.com>
2021-07-09 07:45:34 +0400

    todo list split to 2 components

---
+++ src/pages/components/taskItem.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import taskActions from "../../redux/task/actions";
+
+import { CheckBox } from "../../components/ui-kit/checkBox";
+import { Button } from "../../components/ui-kit/button/Button";
+import { TextInput } from "../../components/ui-kit/textInput/TextInput";
+import { deleteTaskApi, doneTaskApi, editTaskApi } from "../../services/task";
+
+interface IProps {
+  id: number;
+  title: string;
+}
+const TaskItem: React.FC<IProps> = ({ id, title }) => {
+  const [editTaskId, setEditTaskId] = useState(-1);
+  const [newTitle, setNewTitle] = useState("");
+
+  const dispatch = useDispatch();
+
+  const saveHandle = async () => {
+    const data = await editTaskApi(editTaskId, newTitle);
+    dispatch(taskActions.fillTasks(data));
+    setEditTaskId(-1);
+    setNewTitle("");
+  };
+  const isDoneHandle = (id: number) => async () => {
+    const data = await doneTaskApi(id);
+    dispatch(taskActions.fillTasks(data));
+  };
+  const deleteTaskHandle = (id: number) => async () => {
+    if (window.confirm("Are you sure?")) {
+      const data = await deleteTaskApi(id);
+      dispatch(taskActions.fillTasks(data));
+    }
+  };
+  return (
+    <>
+      <CheckBox onChange={isDoneHandle(id)} />
+      {editTaskId !== -1 && id === editTaskId ? (
+        <>
+          <TextInput
+            value={newTitle}
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+          <Button className="save" onClick={saveHandle}>
+            Save
+          </Button>
+        </>
+      ) : (
+        <>
+          <label>{title}</label>
+          <Button
+            className="edit"
+            onClick={() => {
+              setEditTaskId(id);
+              setNewTitle(title);
+            }}
+          >
+            Edit
+          </Button>
+          <Button className="delete" onClick={deleteTaskHandle(id)}>
+            Delete
+          </Button>
+        </>
+      )}
+    </>
+  );
+};
+
+export { TaskItem };
--- src/pages/components/taskList.tsx
+++ src/pages/components/taskList.tsx
@@ -1,73 +1,20 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 import { AppState } from "../../redux/store";
 
-import taskActions from "../../redux/task/actions";
-
-import { CheckBox } from "../../components/ui-kit/checkBox";
-import { Button } from "../../components/ui-kit/button/Button";
-import { TextInput } from "../../components/ui-kit/textInput/TextInput";
-import { deleteTaskApi, doneTaskApi, editTaskApi } from "../../services/task";
+import { TaskItem } from "./taskItem";
 
 const TaskList: React.FC = () => {
-  const [editTaskId, setEditTaskId] = useState(-1);
-  const [newTitle, setNewTitle] = useState("");
-
-  const dispatch = useDispatch();
-
   const { tasks } = useSelector((state: AppState) => state.Task);
 
-  const saveHandle = async () => {
-    const data = await editTaskApi(editTaskId, newTitle);
-    dispatch(taskActions.fillTasks(data));
-    setEditTaskId(-1);
-    setNewTitle("");
-  };
-  const isDoneHandle = (id: number) => async () => {
-    const data = await doneTaskApi(id);
-    dispatch(taskActions.fillTasks(data));
-  };
-  const deleteTaskHandle = (id: number) => async () => {
-    if (window.confirm("Are you sure?")) {
-      const data = await deleteTaskApi(id);
-      dispatch(taskActions.fillTasks(data));
-    }
-  };
   return (
     <ul id="incomplete-tasks">
       {tasks.map(
         (task) =>
           !task.isDone && (
             <li key={task.id}>
-              <CheckBox onChange={isDoneHandle(task.id)} />
-              {editTaskId !== -1 && task.id === editTaskId ? (
-                <>
-                  <TextInput
-                    value={newTitle || task.title}
-                    onChange={(e) => setNewTitle(e.target.value)}
-                  />
-                  <Button className="save" onClick={saveHandle}>
-                    Save
-                  </Button>
-                </>
-              ) : (
-                <>
-                  <label>{task.title}</label>
-                  <Button
-                    className="edit"
-                    onClick={() => setEditTaskId(task.id)}
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                    className="delete"
-                    onClick={deleteTaskHandle(task.id)}
-                  >
-                    Delete
-                  </Button>
-                </>
-              )}
+              <TaskItem id={task.id} title={task.title} />
             </li>
           )
       )}
